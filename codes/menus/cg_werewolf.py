86655d8334dca0c99aeb96a4a4a32ed1bfc9ea5d
Evan Sampson <ecsampson@gmail.com>
2020-04-27 09:10:31 -0700

    CG work
    Need to add a limit to the amount of rites that can be purchased to 7

--- codes/menus/cg_werewolf.py
+++ codes/menus/cg_werewolf.py
@@ -24,7 +24,7 @@ def werewolf_template(caller, raw_string, **kwargs):
                         'Wisdom': 0}
     caller.db.power = { 'Primal Urge' : 1 }
     caller.db.gifts = {}
-    caller.db.rites = {}
+    caller.db.werewolfRites = {}
     text = 'Select Auspice:'
     option_list = []
     auspices_list = search_script_tag('auspice_stat')
@@ -282,42 +282,60 @@ def _remove_gift(caller, raw_string, **kwargs):
 def werewolf_merits(caller, raw_string, **kwargs):
     max = 10
     total = 0
+    rites_list = []
+    rites_points = 0
+    for item in list(caller.db.werewolfRites.keys()):
+        rites_list.append(item)
+        rite = find(item,statclass='werewolf rite')[0]
+        rites_points = rites_points + rite.db.rank
+    if len(rites_list) > 0:
+        rites_list.sort()
     text = 'Primal Urge: ' + str(caller.db.power['Primal Urge']) + \
-           '|/Merits:|/|/'
+           '|/|/Rites:|/|/'
+    for item in rites_list:
+        text = text + item + '|/'
+    text = text + '|/Merits:|/|/'
     for item in caller.db.merits:
         out = item[0]
-        total = total +  item[1]
+        total = total + item[1]
         if len(item[2]) > 0:
             out = out + ' (' + item[2] + ')'
         out = out + ': ' + str(item[1])
         text = text + out + '|/'
     total = total + (caller.get('Primal Urge',statclass='Power') - 1) * 5
+    if rites_points > 2:
+        total = total + rites_points - 2
     text = text + '|/Points remaining: ' + str(max - total)
     option_list = []
     if total < max:
-        option_list.append( {'key' : 'A',
-                             'desc' : 'Add a merit',
+        option_list.append( {'desc' : 'Add a merit',
                              'goto' : ( 'add_merit',
                                         { 'total' : total,
                                          'max' : max} ) } )
+        option_list.append({'desc': 'Add a rite',
+                            'goto': ('add_rite',
+                                     {'total': total,
+                                      'max': max})})
     if max - total > 4:
-        option_list.append ( {'key' : 'I',
-                              'desc' : 'Increase Primal Urge',
+        option_list.append ( {'desc' : 'Increase Primal Urge',
                               'goto' : _increase_power } )
-    if get(caller,'Blood Potency',statclass='Power') > 1:
-        option_list.append ( {'key' : 'D',
-                              'desc' : 'Decrease Primal Urge',
+    if get(caller,'Primal Urge',statclass='Power') > 1:
+        option_list.append ( {'desc' : 'Decrease Primal Urge',
                               'goto' : _decrease_power } )
     if total > 0:
-        option_list.append( {'key' : 'R',
-                             'desc' : 'Remove a merit',
+        option_list.append( {'desc' : 'Remove a merit',
                              'goto' : ('remove_merit',
                                        {'total' : total,
                                         'max' : max} ) } )
-    if total == max:
+    if rites_points > 0:
+        option_list.append({'desc': 'Remove a rite',
+                            'goto': ('remove_rite',
+                                     {'total': total,
+                                      'max': max})})
+    if total == max and rites_points >= 2:
         option_list.append( {'key' : 'P',
                              'desc' : 'Proceed',
-                             'goto' : 'werewolf_gifts'})
+                             'goto' : 'werewolf_finish_cg'})
     options = tuple(option_list)
     return text, options
 
@@ -333,15 +351,15 @@ def _check_merit(caller, raw_string, **kwargs):
     merits = find(strip_control_sequences(raw_string), statclass='Merit')
     if len(merits) < 1:
         caller.msg('|/I can\'t find ' + strip_control_sequences(raw_string))
-        return 'vampire_merits'
+        return 'werewolf_merits'
     elif len(merits) > 1:
         caller.msg('|/Too many matches found')
-        return 'vampire_merits'
+        return 'werewolf_merits'
     else:
         merit = merits[0]
         if merit.db.restricted == True:
             caller.msg('|/That merit is restricted')
-            return 'vampire_merits'
+            return 'werewolf_merits'
         elif len(merit.db.noteRestrictions) == 0:
             return 'get_merit_value', { 'total' : kwargs['total'],
                                    'note' : '',
@@ -376,7 +394,7 @@ def _check_merit_note(caller, raw_string, **kwargs):
                                 'max' : kwargs['max']}
     else:
         caller.msg('|/Invalid note for that merit')
-        return 'vampire_merits'
+        return 'werewolf_merits'
     
 def get_merit_value(caller, raw_string, **kwargs):
     text = 'Enter value:'
@@ -391,23 +409,56 @@ def get_merit_value(caller, raw_string, **kwargs):
 def _check_merit_value(caller, raw_string, **kwargs):
     if not strip_control_sequences(raw_string).isnumeric():
         caller.msg('|/Invalid value')
-        return 'vampire_merits'
+        return 'werewolf_merits'
     else:
         value=int(strip_control_sequences(raw_string)) 
         if value < 1:
             caller.msg('|/Invalid value')
-            return 'vampire_merits'
+            return 'werewolf_merits'
         elif value + kwargs['total'] - kwargs['merit'].get(caller,
                                     subentry=kwargs['note']) >  kwargs['max']:
             caller.msg('|/You don\'t have enough points')
-            return 'vampire_merits'
+            return 'werewolf_merits'
         elif kwargs['merit'].meets_prereqs(caller,value=value,
                                            subentry=kwargs['note']):
             kwargs['merit'].set(caller,value=value,subentry=kwargs['note'])
-            return 'vampire_merits'
+            return 'werewolf_merits'
         else:
             caller.msg('|/You don\'t meet the prerequisites for that merit')
-            return 'vampire_merits'
+            return 'werewolf_merits'
+
+def add_rite(caller, raw_string, **kwargs):
+    text = 'Rite:'
+    options = ( {'key' : '_default',
+                 'goto' : ( _check_rite,
+                            { 'total' : kwargs['total'],
+                             'max' : kwargs['max'] } ) } )
+    return text,options
+
+def _check_rite(caller, raw_string, **kwargs):
+    rites = find(strip_control_sequences(raw_string), statclass='Werewolf Rite')
+    if len(rites) < 1:
+        caller.msg('|/I can\'t find ' + strip_control_sequences(raw_string))
+        return 'werewolf_merits'
+    elif len(rites) > 1:
+        caller.msg('|/Too many matches found')
+        return 'werewolf_merits'
+    else:
+        rite = rites[0]
+        if rite.db.restricted == True:
+            caller.msg('|/That rite is restricted')
+            return 'werewolf_merits'
+        else:
+            value = rite.db.rank
+            if value + kwargs['total']  > kwargs['max']:
+                caller.msg('|/You don\'t have enough points')
+                return 'werewolf_merits'
+            elif rite.meets_prereqs(caller, value=True):
+                rite.set(caller, value=True)
+                return 'werewolf_merits'
+            else:
+                caller.msg('|/You don\'t meet the prerequisites for that rite')
+                return 'werewolf_merits'
 
 def remove_merit(caller, raw_string, **kwargs):
     text = 'Remove which merit:'
@@ -426,161 +477,40 @@ def remove_merit(caller, raw_string, **kwargs):
 
 def _delete_merit(caller, raw_string, **kwargs):
     set(caller, kwargs['entry'], subentry=kwargs['subentry'], value=0)
-    return 'vampire_merits'
+    return 'werewolf_merits'
+
+def remove_rite(caller, raw_string, **kwargs):
+    text = 'Remove which rite:'
+    option_list = []
+    for item in list(caller.db.werewolfRites.keys()).sort():
+        rite = item
+        option_list.append( {'desc' : rite ,
+                             'goto' : ( _delete_rite,
+                                    { 'rite' : rite } ) } )
+    options = tuple(option_list)
+    return text,options
+
+def _delete_rite(caller, raw_string, **kwargs):
+    set(caller, kwargs['rite'], value=False)
+    return 'werewolf_merits'
     
 def _increase_power(caller, raw_string, **kwargs):
     if caller.db.power['Primal Urge'] < 10:
         caller.db.power['Primal Urge'] = caller.db.power['Primal Urge'] + 1
         send_kwargs = kwargs
-    return 'vampire_merits'
+    return 'werewolf_merits'
 
 def _decrease_power(caller, raw_string, **kwargs):
     if caller.db.power['Primal Urge'] > 1:
         caller.db.power['Primal Urge'] = caller.db.power['Primal Urge'] - 1
-    return 'vampire_merits'
-    
-def vampire_disciplines(caller, raw_string, **kwargs):
-    clan_msg = ['|/Clan Disciplines:']
-    out_msg = ['|/Out of Clan Disciplines:']
-    clan_list = []
-    out_list = []
-    clan_count = 0
-    total_count = 0
-    clan = find(caller.db.sphere['Clan'],statclass='Clan')[0]
-    if caller.db.disciplines:
-        disciplines = list(caller.db.disciplines.keys())
-    else:
-        disciplines = []
-    for item in disciplines:
-        value = caller.db.disciplines[item]
-        entry = '|_|_|_|_' + item + ': ' + str(value)
-        if item in clan.db.favored_disciplines:
-            clan_count = clan_count + value
-            clan_list.append(entry)
-        else:
-            out_list.append(entry)
-        total_count = total_count + value
-    
-    if caller.db.coils:
-        coils = list(caller.db.coils.keys())
-    else:
-        coils = []
-    for item in coils:
-        value = caller.db.coils[item]
-        entry = '|_|_|_|_' + item + ': ' + str(value)
-        out_list.append(entry)
-        total_count = total_count + value
-    
-    clan_list.sort()
-    out_list.sort()
-    for item in clan_list:
-        clan_msg.append(item)
-    for item in out_list:
-        out_msg.append(item)
-    text = 'Disciplines: 3 Dots. (2 must be spent on clan disciplines)'
-    for item in clan_msg:
-        text = text + '|/|_|_|_|_' + item
-    for item in out_msg:
-        text = text + '|/|_|_|_|_' + item
-    option_list = []
-    if total_count < 3:
-        option_list.append( { 'key' : 'A',
-                              'desc' : 'Add a discipline or coil',
-                              'goto' : 'vampire_add_discipline' } ) 
-    if len(clan_msg) + len(out_msg) > 2:
-        option_list.append( { 'key' : 'R',
-                              'desc' : 'Remove a discipline or coil',
-                              'goto' : 'vampire_remove_discipline' } ) 
-    if clan_count >= 2 and total_count == 3:
-        option_list.append( { 'key' : 'F',
-                              'desc' : 'Finish character generation',
-                               'goto' : 'vampire_finish_cg' } )
-    options = tuple(option_list)
-    return text,options
-
-def vampire_remove_discipline(caller, raw_string, **kwargs):
-    text = 'Select discipline to remove:'
-    option_list = []
-    if caller.db.disciplines:
-        disciplines = list(caller.db.disciplines.keys())
-    else:
-        disciplines = []
-    disciplines.sort()
-    if caller.db.coils:
-        coils = list(caller.db.coils.keys())
-        coils.sort()
-        for item in coils:
-            disciplines.append(item)
-    for item in disciplines:
-        option_list.append( { 'desc' : item,
-                              'goto' : ( _remove_discipline,
-                                       { 'discipline' : item } ) } )
-    options = tuple(option_list)
-    return text,options
-
-def _remove_discipline(caller, raw_string, **kwargs):
-    if ('Mystery Coil' in caller.db.sphere and 
-        caller.db.sphere['Mystery Coil'] == kwargs['discipline']):
-        caller.msg('|/Removal of favored coil required a reset of all coils')
-        del caller.db.coils
-        del caller.db.sphere['Mystery Coil']
-    else:
-        set(caller,kwargs['discipline'],value=False)
-    return 'vampire_disciplines'
-
-def vampire_add_discipline(caller, raw_string, **kwargs):
-    text = 'Discipline or Coil:'
-    options = ( {'key' : '_default',
-                 'goto' : _check_discipline } )
-    return text,options
-
-def _check_discipline(caller, raw_string, **kwargs):
-    stat = (find(strip_control_sequences(raw_string),statclass='Discipline') + 
-            find(strip_control_sequences(raw_string),statclass='Coil'))
-    if len(stat) < 1:
-        caller.msg('|/I can\'t find ' + strip_control_sequences(raw_string))
-        return 'vampire_disciplines'
-    elif len(stat) > 1:
-        caller.msg('|/Too many matches found')
-        return 'vampire_disciplines'
-    else:
-        stat = stat[0]
-        if stat.db.restricted == True:
-            caller.msg('|/That ' + stat.type() + ' is restricted')
-            return 'vampire_disciplines'
-        else:
-            return 'get_discipline_value', { 'stat' : stat }
-        
-def get_discipline_value(caller, raw_string, **kwargs):
-    text = 'Enter value:'
-    options = ( {'key' : '_default',
-                 'goto' : ( _check_stat_value, 
-                            { 'stat' : kwargs['stat'] } ) } )
-    return text,options
-
-def _check_stat_value(caller, raw_string, **kwargs):
-    if not strip_control_sequences(raw_string).isnumeric():
-        caller.msg('|/Invalid value')
-        return 'vampire_disciplines'
-    else:
-        value=int(strip_control_sequences(raw_string)) 
-        if value < 1:
-            caller.msg('|/Invalid value')
-            return 'vampire_disciplines'
-        elif kwargs['stat'].meets_prereqs(caller,value=value):
-            kwargs['stat'].set(caller,value=value)
-            return 'vampire_disciplines'
-        else:
-            caller.msg('|/You don\'t meet the prerequisites for that '+
-                       kwargs['stat'].type())
-            return 'vampire_disciplines'
+    return 'werewolf_merits'
         
 def quit(caller, raw_string, **kwargs):
     
     text = {'format' : 'suppress'}
     return text,None
 
-def _vampire_finish_cg(caller, raw_string, **kwargs):
+def _werewolf_finish_cg(caller, raw_string, **kwargs):
     caller.cmdset.delete('unfinished_character')
     caller.cmdset.add(
         'codes.commands.character_commands.finished_character',permanent=True)
