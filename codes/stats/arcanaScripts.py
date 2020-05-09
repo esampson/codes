28a8729ab37055356acf04a49b997eab1d4cd6b1
Evan Sampson <ecsampson@gmail.com>
2020-05-08 21:32:59 -0700

    Corrected prereqs for arcanaScripts
    When I first wrote the prereq code I didn't understand how gnosis limited the maximum value of spheres.

--- codes/stats/arcanaScripts.py
+++ codes/stats/arcanaScripts.py
@@ -4,8 +4,11 @@ from codes.data import find
 
 restrict_level = False
 
+sphere_limits = [[3,2], [3,3], [4,3], [4,4], [5,4], [5,5], [5,5], [5,5], [5,5],
+                 [5,5]]
+
 class arcanaScript(codesScript):
-    
+
     def at_script_creation(self):
             self.persistent = True  # will survive reload
             self.db.longname = ''
@@ -14,14 +17,14 @@ class arcanaScript(codesScript):
             self.db.restricted = False
             self.tags.add('stat_data')
             self.tags.add('arcana_stat')
-    
+
     def update(self,longname='', info='', reference='',
                restricted=False):
                 self.db.longname = longname
                 self.db.info = info
                 self.db.reference = reference
                 self.db.restricted = restricted
-        
+
     def get(self, target, subentry=''):
         """
         get
@@ -42,7 +45,7 @@ class arcanaScript(codesScript):
         else:
             result = 0
         return result
-        
+
     def meets_prereqs(self, target, value=0, subentry=''):
         """
         meets_prereqs
@@ -55,19 +58,29 @@ class arcanaScript(codesScript):
         target: The character being checked
         value: The level being checked.
         subentry: Seeming benefits
-        
-        
+
+
         """
+        highest = 0
+        for item in list(target.db.arcana.keys()):
+            if target.db.arcana[item] > highest:
+                highest = target.db.arcana[item]
+        highest_max = sphere_limits[target.get('Gnosis',statclass='Power')+1][0]
+        other_max = sphere_limits[target.get('Gnosis', statclass='Power') + 1][1]
         if target.template().lower() == 'mage':
             order = find(target.get('Order', statclass='Sphere'), statclass='Order')
             if name == order.db.inferior and value > 2 and restrict_level:
                 result = False
             elif name != order.db.inferior and value > 4 and restrict_level:
                 result = False
+            elif value > highest_max:
+                result = False
+            elif value > other_max and highest == highest_max:
+                result = False
             else:
                 result = True
         return result
-    
+
     def cost(self, target, value=True, subentry=''):
         """
         cost
@@ -91,7 +104,7 @@ class arcanaScript(codesScript):
         else:
             result = 4
         return result
-                
+
     def set(self, target, value, subentry=''):
         """
         set
@@ -128,5 +141,4 @@ class arcanaScript(codesScript):
             else:
                 result = False
         return result
-                
-                
+
